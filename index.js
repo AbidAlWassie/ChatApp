695e3792c53b8021e409dfab1db1ad3e501094b4
Abid Al-Wasi <abidalwasi@outlook.com>
2021-01-09 22:13:36 +0600

    updated project

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 // App setup
 let app = express();
-let server = app.listen(4000, function () {
+let server = app.listen(4000, () => {
   console.log("listening to 4000");
 });
 
 // Set static directory
 app.use(express.static("public"));
+
+// socket
+let io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("made socket connection")
+});
